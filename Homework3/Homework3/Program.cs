e7e4bf1ebcbc7e3eab089bc99d60fff4c54c8b66
cscout08 <christopher.j.mcclure@gmail.com>
2016-10-19 21:36:45 -0700

    Homework #3

---
+++ Homework3/Homework3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Factorial
+{
+    static void Main()
+    {
+
+        Console.WriteLine("Please enter a number");
+
+        string str;
+        str = Console.ReadLine();
+
+        int n;
+        n = int.Parse(str);
+
+        int i, fact;
+        fact = n;
+        for (i = n - 1; i >= 1; i--)
+        {
+            fact = fact * i;
+        }
+        Console.WriteLine("Factorial is: " + fact);
+        Console.ReadLine();
+
+    }
+}
