8cc9778a3edb15f42e3278cba930598ed14af1d9
cscout08 <christopher.j.mcclure@gmail.com>
2016-11-09 21:39:11 -0800

    HMWK 4

---
+++ Assignment__4.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Assignment4
+
+{
+
+    static void Main()
+
+    {
+        Console.WriteLine("Please enter a number");
+
+        string str = Console.ReadLine();
+
+        int n = int.Parse(str);  // Gets numbers to calculate
+
+        int Max;
+        int Fact;
+
+
+        Max = n;
+        Fact = 1;
+
+
+        for (int Index = 1; Index <= Max; Index++)
+
+        {
+            Fact = Index * Fact;
+
+            Console.WriteLine("Factorial is {0}", Fact);
+        }
+        Console.ReadLine();
+    }
+}
